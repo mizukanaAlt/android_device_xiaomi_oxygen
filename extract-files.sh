b6b18a5673df2a6288eb3273a55ac95a81aea535
mizukanaAlt <rina301087@gmail.com>
2025-10-29 10:59:31 +0000

    oxygen: libmpbase remove needed libandroid.so

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,9 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/libmpbase.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
         vendor/lib64/lib_fpc_tac_shared.so)
             if ! "${PATCHELF}" --print-needed "${2}" | grep "libshims_binder.so" >/dev/null; then
                 "${PATCHELF}" --add-needed "libshims_binder.so" "${2}"
